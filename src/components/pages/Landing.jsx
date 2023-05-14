eecb3bae4998201f0bf9d31979cca779c68b7e0c
Stanley Diki <stanleydiki@gmail.com>
2023-05-14 21:46:08 +0100

    adding code and styles for the about page

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import About from './About'
 
 export default function Landing() {
   return (
     <div>
-      <h4>landing page</h4>
+      <About />
     </div>
   )
 }
