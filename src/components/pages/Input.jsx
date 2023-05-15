e91157d66c77d02a37b68e6d4e3adde0a0de30a4
Stanley Diki <stanleydiki@gmail.com>
2023-05-15 21:55:59 +0100

    adding code for input page

--- src/components/pages/Input.jsx
+++ src/components/pages/Input.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 export default function Input() {
   return (
-    <form>
-      <h4>form data</h4>
-    </form>
+    <div className='container'>
+      <div className='card'>
+        <div className='card-header'>
+          <h4>add contact</h4>
+        </div>
+          <div className='card-body'>
+            <form>
+              <div className='form-control'>
+                <label htmlFor="name">Name</label>
+                <input 
+                  type='text'
+                  name='name'
+                  id='name'
+                  className='control control-lg'
+                  placeholder='Enter Name'
+                />
+              </div>
+              <div className='form-control'>
+                <label htmlFor="phone">Phone</label>
+                <input 
+                  type='text' 
+                  name='phone' 
+                  id='phone' 
+                  className='control control-lg'
+                  placeholder='Enter Phone' 
+                />
+              </div>
+              <input 
+                type='submit'
+                className='btn btn-light btn-block'
+                value='Add Contact' 
+              />
+            </form>
+          </div>
+      </div>
+    </div>
   )
 }
