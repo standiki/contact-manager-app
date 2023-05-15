c45019d1dc1e8d7f12d1510a0f48d75a7dc56148
Stanley Diki <stanleydiki@gmail.com>
2023-05-15 22:00:22 +0100

    fixed class name

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      <h4>about page</h4>
+    <div className='container'>
+        <div className='about'>
+          <h1>about stanley's contact manager</h1>
+          <p className="lead">
+            a simple app to manage customers contacts details for businesses and organization
+          </p>
+          <p className="sub-lead">version 1.0.0</p>
+        </div>
     </div>
   )
 }
